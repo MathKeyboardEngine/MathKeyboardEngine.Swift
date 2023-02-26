4601fbad68fb44a08584521814c4aa59b919742b
SymboLinker <symbolinker@gmail.com>
2023-02-27 00:36:17 +0100

    refactor moveDown and moveUp

--- Sources/MathKeyboardEngine/KeyboardEngine/Functions/Navigation/moveDown.swift
+++ Sources/MathKeyboardEngine/KeyboardEngine/Functions/Navigation/moveDown.swift
@@ -1,10 +1,7 @@
 public extension KeyboardMemory {
   func moveDown() -> Void {
     var fromPlaceholder = (self.current as? Placeholder) ?? (self.current as! TreeNode).parentPlaceholder!
-    while true {
-      guard let suggestingNode = fromPlaceholder.parentNode else {
-        return
-      }
+    while let suggestingNode = fromPlaceholder.parentNode  {
       if let suggestion = suggestingNode.getMoveDownSuggestion(fromPlaceholder) {
         self.current = suggestion.nodes.last ?? suggestion
         return
--- Sources/MathKeyboardEngine/KeyboardEngine/Functions/Navigation/moveUp.swift
+++ Sources/MathKeyboardEngine/KeyboardEngine/Functions/Navigation/moveUp.swift
@@ -1,10 +1,7 @@
 public extension KeyboardMemory {
   func moveUp() -> Void {
     var fromPlaceholder = (self.current as? Placeholder) ?? (self.current as! TreeNode).parentPlaceholder!
-    while true {
-      guard let suggestingNode = fromPlaceholder.parentNode else {
-        return
-      }
+    while let suggestingNode = fromPlaceholder.parentNode {
       if let suggestion = suggestingNode.getMoveUpSuggestion(fromPlaceholder) {
         self.current = suggestion.nodes.last ?? suggestion
         return
